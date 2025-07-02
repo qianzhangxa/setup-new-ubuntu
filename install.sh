d118b928eabf8a9e052ef1c9c00cee6914caf2bc
Qian Zhang <zhq527725@gmail.com>
2025-07-02 15:47:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -200,6 +200,7 @@ alias dl='docker logs'
 alias db='docker build'
 
 alias date="date '+%Y-%m-%d %H:%M:%S %Z %z'"
+alias svi='sudo -E vim'
 
 export LS_COLORS=$LS_COLORS:'di=1;37;104'
 zstyle ':completion:*' list-colors 'di=1;37;104'
